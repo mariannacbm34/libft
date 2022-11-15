fe6cf7c4deeaec72b4dc9e5071852c36da289725
Marianna Monteiro <113458788+marmonte34@users.noreply.github.com>
2022-11-15 21:55:44 +0000

    libft

--- ft_split.c
+++ ft_split.c
@@ -12,6 +12,30 @@
 
 #include "libft.h"
 
+int  counter (const char *str, char c)
+{
+	int counter;
+	int find;
+	int i;
+
+	find = 0;
+	counter = 0;
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] != c && find == 0)
+		{ 
+			find = 1;
+			counter++;
+		}
+		else if (str[i] == c)
+			find = 0;
+	}
+	return (counter);
+}
+
+char	*word(const char *str, int start, int finish)
+
 char    **ft_split(char const *s, char c)
 {
 
