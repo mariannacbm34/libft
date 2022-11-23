1c046db8a848fa2fc4dd06603d7acebd1d5b5088
marmonte34 <marinnamonteiro34@gmail.com>
2022-11-23 16:29:21 +0000

    libft

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,14 +6,17 @@
 /*   By: marmonte <marmonte@student.42lisboa.com >  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 15:25:55 by marmonte          #+#    #+#             */
-/*   Updated: 2022/11/23 15:09:58 by marmonte         ###   ########.fr       */
+/*   Updated: 2022/11/23 16:28:32 by marmonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	
+	while (lst)
+	{
+		(*f)(lst->content);
+		lst = lst->next;
+	}
 }
-*/
