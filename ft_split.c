6871e35dfbdbd41a74893027a115f5ebba3af7a9
marmonte34 <marinnamonteiro34@gmail.com>
2022-11-23 13:46:51 +0000

    libft

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,44 @@
 /*   By: marmonte <marmonte@student.42lisboa.com >  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:16:21 by marmonte          #+#    #+#             */
-/*   Updated: 2022/11/22 14:13:09 by marmonte         ###   ########.fr       */
+/*   Updated: 2022/11/23 13:46:19 by marmonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
-char	**ft_split(char const *s, char c)
+int	count_words(const char *str, char c)
 {
-	
-}*/
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (str[i])
+	{
+		if (str[i] != c)
+		{
+			word++;
+			while (str[i] != c && str[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (word);
+}
+
+char	*word_dup(char const *str, int start, int finish)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc(sizeof(char) * (finish - start + 1));
+	if (!word)
+		return (NULL);
+	while (start < finish)
+		word[i++] = str[start++];
+	word[i + start] = '\0';
+	return (word);
+}
